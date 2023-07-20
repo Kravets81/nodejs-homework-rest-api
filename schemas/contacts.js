5d67f16405f33b9a0dbcaac6747492da5becd1df
Aleksandr Kravets <skrav@ukr.net>
2023-07-20 16:18:18 +0200

    add changes

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,11 +1,18 @@
 const Joi = require("joi");
 
 const addSchema = Joi.object({
+  _id: Joi.string(),
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchemas = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 module.exports = {
   addSchema,
+  updateFavoriteSchemas,
 };
